9d046b55165e4526bc806b982e31f2e55c726623
Magnus SEO <153587061+magnusseo@users.noreply.github.com>
2023-12-12 16:35:43 +0800

    Add files via upload

---
+++ TC.Lottery.Hack/Properties/SqualrSettings.cs
@@ -0,0 +1,28 @@
+﻿namespace Squalr
+{
+    using System;
+    
+    /// <summary>
+    /// A static class for interfacing with Squalr non-engine settings.
+    /// </summary>
+    public static class SqualrSettings
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether Squalr should check for automatic updates.
+        /// </summary>
+        public static Boolean AutomaticUpdates
+        {
+            get
+            {
+                return Properties.Settings.Default.AutomaticUpdates;
+            }
+
+            set
+            {
+                Properties.Settings.Default.AutomaticUpdates = value;
+            }
+        }
+    }
+    //// End class
+}
+//// End namespace
