8f8453f5d3fb7bd29a602a3df2c9d434bd1fa757
Magnus SEO <153587061+magnusseo@users.noreply.github.com>
2023-12-12 16:20:13 +0800

    Adding back style cop, misc code quality changes

---
+++ TC.Lottery.Cli/CommandHandlers/Process/ProcessCloseOptions.cs
@@ -0,0 +1,27 @@
+﻿namespace Squalr.Cli.CommandHandlers.Process
+{
+    using CommandLine;
+    using System;
+
+    [Verb("close", HelpText = "Detaches from a running process.")]
+    public class ProcessCloseOptions
+    {
+        public Int32 Handle()
+        {
+            if (SessionManager.Session == null)
+            {
+                Console.WriteLine("[Warn] - Not attached to any process.");
+
+                return -1;
+            }
+
+            SessionManager.Session = null;
+
+            Console.WriteLine("Detached from process.");
+
+            return 0;
+        }
+    }
+    //// End class
+}
+//// End namespace
